125c76b40a37ba5a331f6fcb81930dd5d1a7c7de
“Matias <mattmedi08@gmail.com>
2025-04-30 10:15:19 -0300

    Termine el punto 2 del ejercicio 1

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿    using EspacioClases;
+
+            static void MostrarTareas(List<Tarea> lista, string titulo){
+
+            Console.WriteLine($"\n{titulo}");
+            foreach (var tarea in lista)
+            {
+                Console.WriteLine($"ID:{tarea.TareaID} - Descripcion:{tarea.Descripcion} - Duracion:{tarea.Duracion} min");
+            }
+
+            }
+
+    string[] descrip = { "Codear", "Comer", "Rezar", "Trabajar", "Procesar", "estudiar", "practicar", "Dormir", "Vacunarse"};
+
+    List<Tarea> tareasPendientes = new List<Tarea>();
+    List<Tarea> tareasRealizadas = new List<Tarea>();
+
+    Random rnd = new Random();
+
+    int N = rnd.Next(1, 11);
+    int M = 0;
+
+    Console.WriteLine($"Se generaran {N} tareas Pendientes aleatorias. \n");
+
+    for (int i = 0; i < N; i++)
+    {
+        M = rnd.Next(0,9);
+
+        int id = i+1;
+        string descripcion = descrip[M];
+        int duracion = rnd.Next(10, 101);
+
+        Tarea nuevaTarea = new Tarea(id, descripcion,duracion);
+        tareasPendientes.Add(nuevaTarea);
+
+    }
+
+    string respuesta;
+
+    do
+    {
+        
+        MostrarTareas(tareasPendientes,"Tareas Pendientes:");
+
+        Console.WriteLine("\nIngrese el ID de la tarea que desea marcar como realizada: ");
+
+        if (int.TryParse(Console.ReadLine(), out int idSeleccionado))
+        {
+            Tarea tarea = tareasPendientes.Find(t => t.TareaID == idSeleccionado);
+
+            if (tarea != null)
+            {
+                tareasPendientes.Remove(tarea);
+                tareasRealizadas.Add(tarea);
+                Console.WriteLine("Tarea movida a realizadas.");
+
+            }
+            else
+            {
+                Console.WriteLine("No se encontro La tarea con ese ID.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Entrada Invalida.");
+        }
+    
+        Console.Write("\n¿Desea mover otra tarea? (s/n): ");
+        respuesta = Console.ReadLine().ToLower();
+        Console.Clear();
+    
+    } while (respuesta == "s");
+
+
+    MostrarTareas(tareasPendientes,"Tareas Pendientes:");
+    MostrarTareas(tareasRealizadas,"Tareas Realizadas:");
--- ToDo/Tarea.cs
+++ ToDo/Tarea.cs
@@ -1,18 +1,48 @@
 namespace EspacioClases
 {
 
-    public class Tarea {
-        
+    public class Tarea
+    {
+
         public int TareaID { get; set; }
         public string Descripcion { get; set; }
-        public int Duracion { get; set; } // Validar que esté entre 10 y 100
-        
+
+        private int duracion;
+        public int Duracion
+        {
+            get => duracion;
+            set
+            {
+
+                if (value < 10 || value > 100)
+                {
+
+                    duracion = 0;
+
+                }
+                else
+                {
+
+                    duracion = value;
+
+                }
+
+            }
+
+        } // Validar que esté entre 10 y 100
+
         // Puedes añadir un constructor y métodos auxiliares si lo consideras necesario
 
-        
+
+        public Tarea(int tareaID, string descripcion, int duracion)
+        {
+            this.TareaID = tareaID;
+            this.Descripcion = descripcion;
+            this.Duracion = duracion;
+        }
 
 
     }
 
-    
+
 }
